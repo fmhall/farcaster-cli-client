b6aa12e69876916852dfd54ce0e7411cb1f383c6
Mason Hall <masonhall@gmail.com>
2023-03-06 15:08:31 -0800

    v0.1.1

--- README.md
+++ README.md
@@ -1,23 +1,42 @@
----
-title: Farcaster Python bot
-description: A Farcaster bot written in Python
-tags:
-  - python
-  - Farcaster
----
+# farcaster-cli
 
-# Farcaster-py Example
+<div align="center">
 
-This example starts a Farcaster bot using [farcaster-py](https://github.com/a16z/farcaster-py).
+[![Python Version](https://img.shields.io/pypi/pyversions/farcaster-cli.svg)](https://pypi.org/project/farcaster-cli/)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![chat](https://img.shields.io/badge/chat-telegram-blue)](https://t.me/+aW_ucWeBVUZiNThh)
 
-[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template/VoQH_z?referralCode=ifkIie)
+farcaster-cli is a CLI client for the Farcaster protocol<br></br>
 
-## ✨ Features
+</div>
 
-- Python
-- Farcaster-py
+## Installation
 
-## 💁‍♀️ How to use
+```bash
+pip install farcaster-cli
+```
 
-- Install packages using `poetry install`
-- Start the bot using `poetry run python main.py`
+or install with [Poetry](https://python-poetry.org/):
+
+```bash
+poetry add farcaster-cli
+```
+
+## Usage
+
+This client leverages the Warpcast API. [Warpcast](https://warpcast.com/) is one of many Farcaster [clients](https://github.com/a16z/awesome-farcaster#clients). As more APIs are created and hosted by different clients, these will be added to the CLI.
+
+To use the Warpcast API you need to have a Farcaster account. We will use the mnemonic or private key of the Farcaster custody account (not your main wallet) to connect to the API.
+
+First, save your Farcaster mnemonic or private key to a $MNEMONIC environment variable. Now you can initialize the client, and automatically connect.
+
+
+```bash
+export MNEMONIC = <your custody seed phrase here>
+```
+
+```bash
+farcaster-cli $MNEMONIC --watch-all
+```
+
+This subscribes to all recent casts. If you want only the casts of people you follow, remove `--watch-all`.
---
+++ farcaster_cli/main.py
@@ -0,0 +1,65 @@
+import typer
+from farcaster import Warpcast
+from farcaster.models import ApiCast, ApiUser
+from typing import Optional, Set
+from rich import print
+from rich.panel import Panel
+from rich.padding import Padding
+from rich.progress import Progress, SpinnerColumn, TextColumn
+
+cast_link_prefix = "farcaster://casts/"
+user_link_prefix = "farcaster://profiles/"
+
+
+def main(mnemonic: str, watch_all: bool = False):
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        progress.add_task(description="Logging in...", total=None)
+        progress.add_task(description="Reticulating splines...", total=None)
+
+        client = Warpcast(mnemonic=mnemonic)
+
+        following: Set[int] = set()
+        if not watch_all:
+            following_users = client.get_all_following().users
+            following = set([user.fid for user in following_users])
+    print("Ready!")
+
+    for cast in client.stream_casts():
+        if cast:
+            if not watch_all:
+                if cast.author.fid not in following:
+                    continue
+            if cast.text.startswith("recast:farcaster://casts/"):
+                recaster = cast.author
+                cast_hash = cast.text.lstrip("recast:farcaster://casts/")
+                cast = client.get_cast(cast_hash).cast
+            else:
+                recaster = None
+            print_cast(cast, recaster)
+
+
+def print_cast(cast: ApiCast, recaster: Optional[ApiUser] = None) -> None:
+    if recaster:
+        print(
+            f"🔁 [red bold link={user_link_prefix + str(recaster.fid)}]{recaster.username}[/red bold link] recasted:"
+        )
+    parent = ""
+    if cast.parent_hash:
+        parent = f" replying to [purple link={cast_link_prefix + str(cast.parent_hash)}](parent)[/purple link]"
+
+    print(
+        Padding(
+            f"[green bold link={user_link_prefix + str(cast.author.fid)}]{cast.author.username}[/green bold link]{parent} | [purple link={cast_link_prefix + str(cast.hash)}](cast)[/purple link]",
+            (0, 1),
+        )
+    )
+    print(Panel(cast.text))
+    print()
+
+
+def start():
+    typer.run(main)
